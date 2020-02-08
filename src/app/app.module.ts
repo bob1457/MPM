23a8b27835e53376ea392fce08a4e7105872aca4
Bob Yuan <bob.yuan@yahoo.com>
2020-02-07 20:57:55 -0800

    Add reandom banner at page reload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { AppMaterialModule } from './app-material/app-material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    AppMaterialModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+    banner = [
+      'home-banner.jpg',
+      'home-banner1.jpg',
+      'home-banner2.jpg',
+      'home-banner3.jpg',
+      'home-banner4.jpg',
+      'home-banner5.jpg'
+    ];
+
+  bgImg = '';
+  imgPath = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.bgImg =  this.banner[Math.floor(Math.random() * this.banner.length)];
+
+    this.imgPath = '../../assets/images/' + this.bgImg;
+
+    console.log(this.imgPath);
+
+
+  }
+
+}
