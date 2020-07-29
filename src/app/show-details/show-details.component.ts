f4ae8a8db01c9ef987d59be92e77281e77fe36fc
Bob Yuan <bob.yuan@yahoo.com>
2020-07-29 12:50:25 -0700

    Add application form

--- src/app/show-details/show-details.component.ts
+++ src/app/show-details/show-details.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { PropertyListing } from '../core/models/property-listing';
 import { PropertyService } from '../core/services/property.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-show-details',
@@ -15,13 +16,16 @@ export class ShowDetailsComponent implements OnInit {
   id: number ;
 
   listing$: Observable<PropertyListing>;
-  listing: PropertyListing;
+  listing: any; // PropertyListing;
   imgIndex = 0;
 
   serverUrl = 'http://localhost:63899/';
 
+  appForm: FormGroup;
+
   constructor(private location: Location,
               private actRoute: ActivatedRoute,
+              private formBuilder: FormBuilder,
               private propertyService: PropertyService) {
                 this.id = this.actRoute.snapshot.params.id;
                 console.log(this.id);
@@ -31,7 +35,34 @@ export class ShowDetailsComponent implements OnInit {
     this.listing$ =  this.propertyService.GetListingDetails(this.id);
 
     this.propertyService.GetListingDetails(this.id)
-        .subscribe( list => this.listing = list);
+        .subscribe( list => {
+          this.listing = list;
+          console.log('listing', this.listing);
+        });
+
+
+
+    this.appForm = this.formBuilder.group({
+      rentalPropertyId: [],
+      firstName: [''],
+      lastName: [''],
+      contactTel: [''],
+      contactEmail: [''],
+      contactSms: [''],
+      annualIncome: [],
+      numberOfOccupant: [1],
+      withChildren: [false],
+      status: [1],
+      creditRating: [''],
+      empoyedStatus: [''],
+      reasonToMove: ['']
+
+    });
+  }
+
+  onStatusChange(value) {
+    this.appForm.get('empoyedStatus').setValue(value);
+    console.log('t', value);
   }
 
   back() {
@@ -41,6 +72,12 @@ export class ShowDetailsComponent implements OnInit {
   showFullsize(event) {
     this.imgIndex = event;
     console.log('click on image', event);
+  }
+
+  submit() {
+    debugger;
+    this.appForm.get('rentalPropertyId').setValue(this.listing.rentalProperty.id);
+    console.log('form', this.appForm.value);
   }
 
 }
